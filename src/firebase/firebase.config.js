3a7250f7bf246950c6dc4a7e4328afb2e889272b
marufbroh <marufmd775@gmail.com>
2023-05-02 15:33:42 +0600

    hide firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDcnPjrL0PNBKMT6FWieY9-ZKoQYZGg8A4",
-  authDomain: "ph-assignment-10-50026.firebaseapp.com",
-  projectId: "ph-assignment-10-50026",
-  storageBucket: "ph-assignment-10-50026.appspot.com",
-  messagingSenderId: "81527509373",
-  appId: "1:81527509373:web:39b262f4e3750b8842bc14"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
